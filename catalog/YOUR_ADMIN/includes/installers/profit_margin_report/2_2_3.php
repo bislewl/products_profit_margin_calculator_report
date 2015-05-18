ce650ea61eb33328c59d851eaeacdab1a3f9c77a
Larry Bislew <bislewl@gmail.com>
2015-05-18 13:48:36 -0500

    update docs to 2.2.3, updated installer to 2.2.3

---
+++ catalog/YOUR_ADMIN/includes/installers/profit_margin_report/2_2_3.php
@@ -0,0 +1,14 @@
+<?php
+
+/* 
+ * 
+ * @package products_profit_margin_calculator_report
+ * @copyright Copyright 2003-2015 ZenCart.Codes a Pro-Webs Company
+ * @copyright Copyright 2003-2015 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @filename 2_2_3.php
+ * @file created 2015-05-18 1:47:15 PM
+ * 
+ */
+
