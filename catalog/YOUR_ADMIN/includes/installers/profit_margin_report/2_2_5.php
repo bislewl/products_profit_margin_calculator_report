f58888e750351868d6392a51849285cd25099588
bislewl <bislewl@gmail.com>
2017-06-16 11:24:05 -0500

    Update dos, uninstall.sql, add version file

---
+++ catalog/YOUR_ADMIN/includes/installers/profit_margin_report/2_2_5.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  2_2_5.php
+ *
+ * @copyright Copyright 2003-2017 Zen Cart Development Team
+ * @copyright Portions Copyright 2003 osCommerce
+ * @license http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
+ * @version Author: bislewl  6/15/2017 3:35 PM Modified in products_profit_margin_calculator_report
+ */
+
